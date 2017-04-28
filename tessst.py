332f0fba3fa7303b4975109ed499b371821a8c29
Tkaya <tkaya@students.uni-mail.de>
2017-04-28 20:33:03 +0200

    Alle eigenen Aufgaben erledigt + Menu und unten die Statusbar mit Statustips für Menue einstellungen. Reset zu Standart und Variable Größen ( hab im Wahn des codens nicht beachtet das dies Aarons Aufgaben waren)

--- tessst.py
+++ tessst.py
@@ -5,10 +5,12 @@ from PyQt5 import QtGui as qg
 from PyQt5 import QtCore as qc
 
 
-color1 =  0xff50B4D8
-color2 =  0xffFFBC46
-color3 = 0xffffffff
-color4 = 0xff000000
+color1 = 0xff50B4D8     # blue
+color2 = 0xffFFBC46     # orange
+color3 = 0xffffffff     # white
+color4 = 0xff000000     # black
+color5 = 0xff00BFFF    # dark blueh
+
 class Snake():
     def __init__(self):
         self.x = 1
@@ -16,6 +18,7 @@ class Snake():
         self.movex = 1
         self. movey = 0
         self.point = [(0,0), (0, 1), (0, 2),(0, 3)]
+        self.eatennode = []
         self.node = []
         self.loose = False
         self.count = 0
@@ -37,12 +40,17 @@ class Snake():
         self.node = []
         self.loose = False
         self.won = False
-        enablebtn()
+        self.pause = True
+        timer.start()
+        enableAll()
 
     def drawSnake(self):
+        import random
+
         if not self.loose:
+            self.deleatennode()
             btn.setEnabled(False)
-            bild = qg.QImage(30, 30, qg.QImage.Format_RGB32)
+            bild = qg.QImage(feldbreite, feldbreite, qg.QImage.Format_RGB32)
             if self.pause:
                 bild.fill(qc.Qt.white)
             else:
@@ -53,7 +61,11 @@ class Snake():
             else:
                 for i in self.point:
                     bild.setPixel(i[0], i[1], color1)
-            self.addNode()
+            if len(self.eatennode) != 0:
+                bild.setPixel(self.eatennode[0], self.eatennode[1], color5)
+            z = random.randint(0,10000000)
+            if z < chance:
+                self.addNode()
             self.NodeEat()
             if len(self.node) != 0:
                 if self.pause:
@@ -61,9 +73,9 @@ class Snake():
                 else:
                     bild.setPixel(self.node[0][0], self.node[0][1], color2)
             pixmap = qg.QPixmap.fromImage(bild)
-            scaledpixmap = pixmap.scaled(600, 600, qc.Qt.KeepAspectRatio)
+            scaledpixmap = pixmap.scaled(Zoom, Zoom, qc.Qt.KeepAspectRatio)
             display.setPixmap(scaledpixmap)
-            display.show()
+            main.show()
         else:
             timer.stop()
             btn.setEnabled(True)
@@ -71,10 +83,10 @@ class Snake():
                 self.wonText()
             else:
                 self.lostText()
-            bild = qg.QImage(30, 30, qg.QImage.Format_RGB32)
+            bild = qg.QImage(feldbreite, feldbreite, qg.QImage.Format_RGB32)
             bild.fill(qc.Qt.white)
             pixmap = qg.QPixmap.fromImage(bild)
-            scaledpixmap = pixmap.scaled(600, 600, qc.Qt.KeepAspectRatio)
+            scaledpixmap = pixmap.scaled(Zoom, Zoom, qc.Qt.KeepAspectRatio)
             display.setPixmap(scaledpixmap)
             display.show()
             
@@ -97,13 +109,13 @@ class Snake():
             self.lose()
             x = self.point[0][0] + self.movex
             y = self.point[0][1] + self.movey
-            if x > 29:
+            if x > feldbreite -1:
                 x = 0
             if x < 0:
-                x = 29
+                x = feldbreite -1
             if y < 0:
-                y = 29
-            if y > 29:
+                y = feldbreite -1
+            if y > feldbreite -1:
                 y = 0
             self.point.insert(0, (x, y))
             self.point.__delitem__(-1)
@@ -118,14 +130,22 @@ class Snake():
     def addNode(self):
         import random
         if len(self.node) == 0:
-            x = random.randint(0, 29)
-            y = random.randint(0, 29)
-            self.node = [(x,y)]
+            x = random.randint(0, feldbreite -1)
+            y = random.randint(0, feldbreite -1)
+            if not((x,y) in self.point):
+                self.node = [(x,y)]
+
+    def deleatennode(self):
+        if self.eatennode == self.point[-1]:
+            self.eatennode = []
+
 
     def NodeEat(self):
-        if self.point[0] == self.node[0]:
-            self.node = []
-            self.grow()
+        if len(self.node) != 0:
+            if self.point[0] == self.node[0]:
+                self.eatennode = self.node[0]
+                self.node = []
+                self.grow()
 
     def lose(self):
         for i in self.point[1:]:
@@ -207,23 +227,34 @@ class TastenTest(qw.QWidget):
         if e.key() == qc.Qt.Key_Space:
             enablebtn()
 
-app = qw.QApplication(sys.argv)
-box = qw.QVBoxLayout()
-grid = qw.QGridLayout()
-ex = TastenTest()
-display = qw.QLabel()
-snake = Snake()
-snake.pause = True
-btn = qw.QPushButton("Neustart")
-btn.setEnabled(False)
-btn.pressed.connect(snake.restart)
 
-def menue():
-    timer.setInterval(1/e1.value() * 300)
+
+def menue():    # verwaltet alle Menue Funktionen
+    global chance, player_name, feldbreite, Zoom
+    timer.setInterval(1/e1.value() * 300)       # Geschwindigkeit
+    chance = e5.value() * 100000        # Fruchtwahrscheinlichkeit
+    player_name = e7.text()  # SPieler Name für den Highscore
+    feldbreite = e9.value()
+    Zoom = (2 + e11.value()) * 200
 
 def enablebtn():
     e2.setEnabled(True)
     e3.setEnabled(True)
+    e4.setEnabled(True)
+
+def enableAll():
+    e1.setEnabled(True)
+    e2.setEnabled(True)
+    e3.setEnabled(True)
+    e4.setEnabled(True)
+    e5.setEnabled(True)
+    e6.setEnabled(True)
+    e7.setEnabled(True)
+    e8.setEnabled(True)
+    e9.setEnabled(True)
+    e10.setEnabled(True)
+    e11.setEnabled(True)
+
 
 def pauseIt():
     snake.pause = True
@@ -233,34 +264,96 @@ def startIt():
     e1.setEnabled(False)
     e2.setEnabled(False)
     e3.setEnabled(False)
+    e4.setEnabled(False)
+    e5.setEnabled(False)
+    e7.setEnabled(False)
+    e9.setEnabled(False)
+    e11.setEnabled(False)
     enablebtn()
 
+app = qw.QApplication(sys.argv)
+main = qw.QMainWindow()
+main.setWindowTitle("Sssssnake")
+main.resize(800, 600)
+
+ex = TastenTest()
+display = qw.QLabel()
+snake = Snake()
+snake.pause = True
+
+feldbreite = 10
+Zoom = 600
+chance = 1000000
+
+btn = qw.QPushButton("Neustart")
+btn.setEnabled(False)
+btn.pressed.connect(snake.restart)
+
+e0 = qw.QLabel("Speed:")        # Geschwindigkeitseinstellungen
 e1 = qw.QSpinBox()
 e1.setMinimum(1)
 e1.setMaximum(8)
 
-e0 = qw.QLabel("Speed:")
-
-e2 = qw.QPushButton("Pause")
+e2 = qw.QPushButton("Pause")    # Pause Button
 e2.clicked.connect(pauseIt)
 
-e3 = qw.QPushButton("Start")
+e3 = qw.QPushButton("Start")    # Start Button
 e3.clicked.connect(startIt)
 
+e4 = qw.QLabel("Fruchtwahrscheinlichkeit in %:")
+e5 = qw.QSpinBox()
+e5.setMinimum(0)
+e5.setValue(50)
+e5.setMaximum(100)
 
-grid.addWidget(e2, 1, 0)
-grid.addWidget(e3, 1, 1)
-grid.addWidget(e0, 0,0)
-grid.addWidget(e1,0,1)
-grid.addWidget(btn, 1,4)
-grid.addWidget(display, 0,4)
-ex.setLayout(grid)
-ex.setLayout(grid)
+e6 = qw.QLabel("Spieler Name")
+e7 = qw.QLineEdit()
+
+e8 = qw.QLabel("Feldbreite")
+e9 = qw.QSpinBox()
+e9.setMinimum(10)
+e9.setMaximum(30)
+
+e10 = qw.QLabel("Zoom")
+e11 = qw.QSpinBox()
+e11.setMinimum(1)
+e11.setMaximum(3)
+
+snake.addNode()
+form = qw.QFormLayout()     # Whole Screen
+form2 = qw.QFormLayout()    # Settings Screen
+
+
+form2.addRow(e6, e7)
+form2.addRow(e8, e9)
+form2.addRow(e10, e11)
+form2.addRow(e0, e1)
+form2.addRow(e4, e5)
+form2.addRow(e2, e3)
+#form2.addRow(HIGHSCORE BUTTON)
+form2.addRow(btn)
+form.addRow(display, form2)
+
+ex.setLayout(form)
+main.setCentralWidget(ex)
+menu = main.menuBar()
+menu.setNativeMenuBar(False)
+m1 = menu.addMenu("Spiel")
+m1a = m1.addAction("Neustarten")
+m1a.setStatusTip("Startet das Spiel neu")
+m1a.triggered.connect(lambda: snake.restart())
+m1.addSeparator()
+m1b = m1.addAction("Beenden")
+m1b.triggered.connect(lambda: main.close())
+m1b.setStatusTip("Beendet das Spiel")
+stat = main.statusBar()
+
 timer = qc.QTimer()
 timer.start(0)
 timer.setInterval(100)
 timer.timeout.connect(snake.moveIt)
 timer.timeout.connect(snake.drawSnake)
 timer.timeout.connect(menue)
+
 
 sys.exit(app.exec_())
