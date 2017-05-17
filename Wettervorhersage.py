661c53fdc8941a0c6130132ec9b061e69b338eb8
Hino <hinotokami@gmail.com>
2017-05-17 10:54:15 +0200

    comments

--- Wettervorhersage.py
+++ Wettervorhersage.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Einlesen der Daten aus der Datei Rohdaten.txt
-tabelle =np.loadtxt("Rohdaten.txt")
-# extrahieren der passenden Spalten
-temp = tabelle[:,6]                 # Temperatur Werte
-nied = tabelle[:,12]                # Niederschlagswerte
+# reading data from data file
+table =np.loadtxt("Rohdaten.txt")
+# extract the respective coloumns
+temp = table[:,6]                 # temperature
+rain = table[:,12]                # rainfall
 
 
-#   Datensatz für das Lagrange Polynom aufteilen
-#   Gleichzeitig Erstellen eines Train und Test Satzes für die Cross Validation
+#   splitting dataset for langrange polynome
+#   train and test sets for cross validation
 temp2 = []
 temp2x = []
 for i in range(0, len(temp)+1, 75):
@@ -24,77 +24,73 @@ for i in range(0, len(temp)):
 
 def lagrange(x, xs, ys):
     """
-    eingabe: Stelle der auswertung und x-Werte , y-Werte des Datensatzes
+    input: dataset values xs and ys, point of evaluation x
     """
     def basis(j):
         import functools
         import operator
         p = [(x - xs[m])/(xs[j] - xs[m]) for m in range(k) if m != j]
         return functools.reduce(operator.mul , p)
-    assert len(xs) != 0 and (len(xs) == len(ys)) , "x and y darf nicht leer sein"
+    assert len(xs) != 0 and (len(xs) == len(ys)) , "x and y cannot be empty"
     k = len(xs)
     return sum(basis(j) * ys[j] for j in range(k))
 
 temp3 = []
-# erstellen der x,y Werte für das Lagrange Polynom
+# get values for lagrange
 for i in range(0,500):
     temp3.append(lagrange(i, temp2x, temp2))
 
 
 
-# Mittelwert
-tempmiddle = []         # y werte
-tempmiddlex = []        # x werte
+# mean values
+tempmiddle = []         # y values
+tempmiddlex = []        # x values
 for i in range(50, len(temp), 50):
     tempmiddle.append(np.mean(temp[i - 50:i]))
     tempmiddlex.append((i + (i - 50)) / 2)
 
-# laufender Mittelwert
+# running averages
 tempmidrun = []
 tempmidrunx = []
-# for Schleife fängt bei 50 an und geht 1er Schritte
 for i in range(50, len(temp)):
-    tempmidrun.append(np.mean(temp[i - 50:i]))      # Sliced immer 50er Fenster and Werten
-    tempmidrunx.append((i + (i - 50)) / 2)          # appended mittelwert und x wert
+    tempmidrun.append(np.mean(temp[i - 50:i]))      # slice intervals
+    tempmidrunx.append((i + (i - 50)) / 2)          # appended mean
 
-# erstellen der subplots
 fig, ax1 = plt.subplots()
-# statt plt.plot nun ax1.plot
 
-# aus xlabel und xlim wird set_xlabel und set_xlim
-ax1.set_xlabel("Zeitpunkt")
+# axis modification
+ax1.set_xlabel("Time")
 ax1.set_ylabel("Temp")
-# Färben der y−Achse
 ax1.tick_params("y", colors="r")
 ax1.plot(temp, "r")
 
-# plottet Mittelwert und den Laufenden Mittelwert
+# plot mean and running average
 plt.plot(tempmiddlex, tempmiddle, "k--", color="r")
 plt.plot(tempmidrunx, tempmidrun, "k--", color="g")
 
-# plottet lagrange
+# plot lagrange
 plt.plot(temp3, "k--")
 
-# lineare regression  mithilfe der im Aufgabenteil gegebenen formel
+# linear regression
 x2 = np.arange(0,500)
-A = np.vstack([x2, np.ones(len(temp))]).T      # transponiert die x-Achse mit einsen
-m, c = np.linalg.lstsq(A, temp)[0]              # berechnet mittels Numpy Befehl m und c
-plt.plot(x2, m*x2 +c, "r", label="Fitted Line")    # plottet die Gleichung m*x+c
+A = np.vstack([x2, np.ones(len(temp))]).T      # transpose x axis
+m, c = np.linalg.lstsq(A, temp)[0]              # get m, c
+plt.plot(x2, m*x2 +c, "r", label="Fitted Line")    # plot graph
 
-# Moving least Squares mittels linear Reg
+# moving least squares
 movey = []
-for i in range(450):            # Fensterbreite 50
+for i in range(450):            # chunks of width 50
     A1 = np.vstack([x2[i:i+50], np.ones(50)]).T
     m1, c1 = np.linalg.lstsq(A1, temp[i:i+50])[0]
     movey.append(m1*((i+i+50)/2) + c1)
 
-plt.plot([i+50/2 for i in range(len(movey))], movey)        # X-Achse Anpassung da Fensterbreite = 50
+plt.plot([i+50/2 for i in range(len(movey))], movey)        # adjust axis
 
 def polyfit(x2, temp):
     """
-    polyfit Methode - Polynomielle Regression
-    Eingabe : x-Werte und Y-Werte
-    Ausgabe : [X-Werte] , [Y-Werte]
+    polyfit method - polynomial regression
+    input : data values
+    output: fitting values
     """
     coeff = np.polyfit(x2, temp, 5)
     poly = np.poly1d(coeff)
@@ -102,31 +98,31 @@ def polyfit(x2, temp):
     poly2 = np.linspace(0,500,500)
     return poly2, poly3
 
-p2, p3 = polyfit(x2, temp)      # Polynomielle Reg über den Kompletten Datensatz
+p2, p3 = polyfit(x2, temp)      # polyfit for complete  data
 plt.plot(p2, p3, "--", color="k")
 
-# zweite y−Achse (mit der gleichen x−Achse)
+# second y axis
 ax2 = ax1.twinx()
-ax2.set_ylabel("Niederschlag")
+ax2.set_ylabel("Rainfall")
 ax2.tick_params("y")
-ax2.plot(nied, "b .")
+ax2.plot(rain, "b .")
 def cross(trainx, trainy, temp):
     """
-    :param temp3:  erstellter datensatz datensatz Y-Werte
-    :param temp:  kompletter datensatz
-    :param temp2x: train datensatz  - X-Werte
-    :return: Qualität E
+    :param temp3:  selected data Y
+    :param temp:  complete data
+    :param temp2x: training data X
+    :return: quality E
     """
     e = 0
     for i in range(len(trainy)):
-        if i not in trainx:                     # überspringt die für den Train Datensatz verwendete Daten
-            e += (trainy[i] - temp[i])**2       # Berechnung aus der Aufgabenstellung
+        if i not in trainx:                     # use non-training data
+            e += (trainy[i] - temp[i])**2
     e = e/(len(temp)- len(trainx))
     return e
 
 eLagrange = cross(temp2x, temp3, temp)
 
-# testfälle für den Polyfit
+# test cases for polyfit
 p5, p6 = polyfit(x2[::50], temp[::50])
 p5train = [i for i in range(500) if i%50 == 0]
 
@@ -135,7 +131,7 @@ print(eLagrange)
 print(ePolyReg)
 
 """
-# Gitter Für den Plot
+# sections for plot
 for i in range(10):
     plt.axhline(5*i, color="b", alpha=2)
     plt.axvline(50*i, color="b", alpha=2)
